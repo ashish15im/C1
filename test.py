a765d58a525ad97a17a4b9ad8052c720befa02d6
ashish <ashish15im@gmail.com>
2025-01-14 03:27:06 +0530

    new

---
+++ test.py
@@ -0,0 +1,10 @@
+# Function to add two strings
+def add_strings(str1, str2):
+    return str1 + str2
+
+# Example usage
+string1 = "Hello, "
+string2 = "World!"
+result = add_strings(string1, string2)
+
+print("Result:", result)
